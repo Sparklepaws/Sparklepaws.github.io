d1ac5d366088e6b0fd217de0d5c232148a24e998
Star Cherrix <wolfcherrix@gmail.com>
2017-02-21 23:07:54 -0500

    Updates

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 function makeray (){
+	if (document.getElementById("myButton").innerHTML = "Reset"){
+			location.reload();
+			}
 	var set1 = [];
 	var set2 = [];
 	var sum1 = 0;
@@ -7,8 +10,6 @@ function makeray (){
 	name_stuff = document.getElementsByClassName('name');
 	var descArray = [].map.call(mmr_stuff, function(input) {return input.value;});
 	var nameArray = [].map.call(name_stuff, function(input) {return input.value;});
-	console.log(nameArray);
-	console.log(descArray);
 	var Data = { 
 	};
 	for (i = 0; i <= 10; i++){
@@ -32,14 +33,14 @@ function makeray (){
   	set2.push(descArray[1]);
   	descArray.splice(0,2);
   
-  	for(var i = 0; i < descArray.length-2; i+= 2){
+  	for(var i = 0; i < descArray.length; i+= 2){
       if(sum1 + descArray[i] > sum2 + descArray[i]){
         sum2 += descArray[i];
         sum1 += descArray[i+1];
         set1.push(descArray[i+1]);
         set2.push(descArray[i]);
       }
-      if(sum1 + descArray[i] < sum2 + descArray[i]){
+      else if(sum1 + descArray[i] < sum2 + descArray[i]){
         sum1 += descArray[i];
         sum2 += descArray[i+1];
         set1.push(descArray[i]);
@@ -52,9 +53,18 @@ function makeray (){
         set2.push(descArray[i+1]);
       }
   }
-		for (i = 0; i <= 5; i++){
-			console.log(Data[set1[i]]);
-			console.log(set1[i]);
+		set1.sort(function(a, b){return b-a});
+		set2.sort(function(a, b){return b-a});
+		console.log(set1);
+		console.log(set2);
+		difference = Math.abs(sum1 - sum2);
+		for (i = 0; i < 5; i++){
+			document.getElementById("a"+i).innerHTML = Data[set1[i]];
+			document.getElementById("b"+i).innerHTML = Data[set2[i]];
 		}
-	}
+		document.getElementById("team1title").innerHTML = ["Team 1 Players"];
+		document.getElementById("team2title").innerHTML = ["Team 2 Players"];
+		document.getElementById("difference").innerHTML = ["Total MMR Difference: " + difference];
+		document.getElementById('myButton').innerHTML = "Reset";
+}
 }
